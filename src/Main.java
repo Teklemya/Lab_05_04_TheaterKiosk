fff4d9c7110781adf20b09dcdbbe1d6d99ab420a
yishak <Teklemya@mail.uc.edu>
2022-09-22 14:28:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 169;
+
+        if (age >= 21)
+        {
+            System.out.println("Wooo!! You are old enough to get a wristband ");
+        }
+    }
+}
